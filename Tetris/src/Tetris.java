be8f2d8c42252aa767abfc05b5f9de43d4162146
Quinn Montgomery <quinninthetardis@gmail.com>
2019-03-19 17:23:16 -0400

    started work on diferent buttons on the main screen

--- Tetris/src/Tetris.java
+++ Tetris/src/Tetris.java
@@ -31,19 +31,112 @@ public class Tetris extends Application{
 		btnS.setText("Start!");
 		btnS.setLayoutX(width/2.43);
 		btnS.setLayoutY(hight/9);
+		btnS.setOnAction(new EventHandler<ActionEvent>() {
+			
+			public void handle(ActionEvent event) {
+				newGame(primaryStage);
+			}
+			
+		});
 		
 		final Button btnC = new Button();
 		btnC.setText("control");
 		btnC.setLayoutX(width/2.5);
 		btnC.setLayoutY((hight/9)*2);
-
+		btnC.setOnAction(new EventHandler<ActionEvent>() {
+			
+			public void handle(ActionEvent event) {
+				showControls(primaryStage);
+			}
+			
+		});
+		
+		final Button btnP = new Button();
+		btnP.setText("pieces");
+		btnP.setLayoutX(width/2.5);
+		btnP.setLayoutY((hight/9)*3);
+		btnP.setOnAction(new EventHandler<ActionEvent>() {
+			
+			public void handle(ActionEvent event) {
+				System.exit(0);
+			}
+			
+		});
+		
+		final Button btnE = new Button();
+		btnE.setText("exit");
+		btnE.setLayoutX(width/2.5);
+		btnE.setLayoutY((hight/9)*4);
+		btnE.setOnAction(new EventHandler<ActionEvent>() {
+			
+			public void handle(ActionEvent event) {
+				System.exit(0);
+			}
+			
+		});
+		
 		root.getChildren().add(btnS);
 		root.getChildren().add(btnC);
+		root.getChildren().add(btnP);
+		root.getChildren().add(btnE);
+		
+		primaryStage.setResizable(false);
 		
 		primaryStage.setScene(new Scene(root, width, hight));
 		primaryStage.show();
 		
 		
+	}
+	
+	/**
+	 * starts a new game
+	 * 
+	 * @param primaryStage
+	 */
+	public void newGame(Stage primaryStage) {
+		
+		final int width = 300;
+		final int hight = 500;
+		
+		primaryStage.setTitle("Tetris");
+		
+		final Pane root = new Pane();
+		
+		primaryStage.setScene(new Scene(root, width, hight));
+		primaryStage.show();
+	}
+	
+	
+	/**
+	 * shows Controls
+	 * 
+	 * @param primaryStage
+	 */
+	public void showControls(Stage primaryStage) {
+		
+		final int width = 300;
+		final int hight = 500;
+		
+		primaryStage.setTitle("Tetris");
+		
+		final Pane root = new Pane();
+		
+		final Button btnB = new Button();
+		btnB.setText("go back");
+		btnB.setLayoutX(width/2.5);
+		btnB.setLayoutY((hight/9)*6);
+		btnB.setOnAction(new EventHandler<ActionEvent>() {
+			
+			public void handle(ActionEvent event) {
+				start(primaryStage);
+			}
+			
+		});
+		
+		root.getChildren().add(btnB);
+		
+		primaryStage.setScene(new Scene(root, width, hight));
+		primaryStage.show();
 	}
 	
 
